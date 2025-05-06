34d6d4c752cd57b4deb82c6ae9aba238237f4fd1
yashwanthm3012 <dev.yashwanthm3012@gmail.com>
2025-05-06 17:18:56 +0000

    files

--- client/main.go
+++ client/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,9 +10,24 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Post("/upload", func(c *fiber.Ctx) error {
+		// Get file from form-data key "file"
+		file, err := c.FormFile("file")
+		if err != nil {
+			return c.Status(400).SendString("No file found in form-data")
+		}
+
+		// Create uploads directory if it doesn't exist
+		os.MkdirAll("./uploads", os.ModePerm)
+
+		// Save file
+		savePath := fmt.Sprintf("./uploads/%s", file.Filename)
+		if err := c.SaveFile(file, savePath); err != nil {
+			return c.Status(500).SendString("Error saving file: " + err.Error())
+		}
+
+		return c.SendString("File uploaded successfully")
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	app.Listen(":3000")
 }
