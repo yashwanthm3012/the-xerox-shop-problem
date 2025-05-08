41be428324e801886796abc77c9d38cfe4b155b3
yashwanthm3012 <dev.yashwanthm3012@gmail.com>
2025-05-08 08:35:26 +0000

    Frontend

---
+++ client-front/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FileUpload from "./FileUpload";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Upload Print Request</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
