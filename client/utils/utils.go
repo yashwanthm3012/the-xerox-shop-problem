69fff717476168caf37eca06abc0ec3d893023ae
yashwanthm3012 <dev.yashwanthm3012@gmail.com>
2025-05-08 09:09:52 +0000

    helper func

---
+++ client/utils/utils.go
@@ -0,0 +1,85 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+// mergePageRanges parses "1,2-4,7" → []int{1,2,3,4,7}
+func MergePageRanges(ranges string) ([]int, error) {
+	var result []int
+	parts := strings.Split(ranges, ",")
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			continue
+		}
+		if strings.Contains(part, "-") {
+			bounds := strings.Split(part, "-")
+			if len(bounds) != 2 {
+				return nil, fmt.Errorf("invalid range: %s", part)
+			}
+			start, err1 := strconv.Atoi(bounds[0])
+			end, err2 := strconv.Atoi(bounds[1])
+			if err1 != nil || err2 != nil || start > end {
+				return nil, fmt.Errorf("invalid range: %s", part)
+			}
+			for i := start; i <= end; i++ {
+				result = append(result, i)
+			}
+		} else {
+			num, err := strconv.Atoi(part)
+			if err != nil {
+				return nil, fmt.Errorf("invalid number: %s", part)
+			}
+			result = append(result, num)
+		}
+	}
+	return result, nil
+}
+
+// forwardToShopStorage sends the file to another microservice via POST
+func ForwardToShopStorage(filePath, fileName string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(fileName))
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(part, file); err != nil {
+		return err
+	}
+	writer.Close()
+
+	req, err := http.NewRequest("POST", "http://localhost:4000/store", body)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("storage service responded with status: %s", resp.Status)
+	}
+
+	return nil
+}
